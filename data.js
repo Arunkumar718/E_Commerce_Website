e98fdbf39f21a484b1b5d77238468a397914842a
Arunkumar718 <147243942+Arunkumar718@users.noreply.github.com>
2025-09-15 13:21:32 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,41 @@
+export const products = [
+    { id: 1, name: "Noise-Cancelling Headphones", price: 199.99, image: "https://www-cdn.bigcommerce.com/assets/quote-image-stock-photo-headphones-generic.png", category: "Electronics" },
+    { id: 2, name: "Vintage Leather Journal", price: 25.50, image: "https://images-na.ssl-images-amazon.com/images/I/81J1amrqD8L._AC_SL1500_.jpg", category: "Books" },
+    { id: 3, name: "Smart TV 55-inch 4K", price: 899.99, image: "https://a-static.mlcdn.com.br/1500x1500/smart-tv-crystal-uhd-4k-led-55-samsung-55tu8000-wi-fi-bluetooth-hdr-3-hdmi-2-usb/magazineluiza/225605800/e32aa79079e21db1b00d012aa94676f9.jpg", category: "Electronics" },
+    { id: 4, name: "Wireless Bluetooth Speaker", price: 75.00, image: "https://i5.walmartimages.com/asr/cdaaab64-5ea7-4d8f-969f-cff666244723.ff62693361b001ebbaf41145d23c0f13.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff", category: "Electronics" },
+    { id: 5, name: "Cozy Wool Sweater", price: 45.99, image: "https://www.realsimple.com/thmb/rcIzj29foJmrq21vxWSczkcubwM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/quince-mongolian-cashmere-crewneck-sweater-tout-ac534d3d75a94c48bca0d3574f2a9479.jpg", category: "Clothing" },
+    { id: 6, name: "Classic Denim Jacket", price: 65.00, image: "https://www.outfittrends.com/wp-content/uploads/2017/02/Denim-jacket-with-black-pants.jpeg", category: "Clothing" },
+    { id: 7, name: "The Alchemist by Paulo Coelho", price: 15.00, image: "https://c8.alamy.com/comp/C9128E/the-alchemist-by-paulo-coelho-C9128E.jpg", category: "Books" },
+    { id: 8, name: "Digital Camera", price: 450.00, image: "https://www.bhphotovideo.com/images/images2500x2500/nikon_13302_d7100_dslr_camera_with_1005009.jpg", category: "Electronics" },
+    { id: 9, name: "Running Shoes", price: 89.99, image: "https://media.gq.com/photos/63eba1b2275d2fef78a425c2/16:9/w_2560%2Cc_limit/nike-running-shoes-streakfly-invincible.jpg", category: "Clothing" },
+    { id: 10, name: "Smartphone X", price: 799.00, image: "https://m.media-amazon.com/images/I/61rIWBU+7qL._AC_SL1500_.jpg", category: "Electronics" },
+];
+
+export const categories = ["All", "Clothing", "Electronics", "Books"];
+export const reviews = [
+  {
+    name: "John",
+    title: "Satisfied Customer",
+    text: "I had a great experience shopping here! Fast delivery and excellent service.",
+    image: "https://i.pinimg.com/originals/a1/49/ca/a149ca308184a8984666979a6b1c4cb7.jpg"
+  },
+  {
+    name: "Roshni ",
+    title: "Happy Shopper",
+    text: "Amazing products and very reliable service. Highly recommended!",
+    image: "https://static.vecteezy.com/system/resources/previews/024/344/087/large_2x/business-woman-isolated-illustration-ai-generative-free-png.png"
+  }
+];
+
+export const teamMembers = [
+    { name: "Virat", title: "Founder & CEO", image: "https://st1.photogallery.ind.sh/wp-content/uploads/indiacom/virat-kohli-black-co-ord-suit-with-a-tee-202410-1730044693.jpg?impolicy=Medium_Widthonly&w=700" },
+    { name: " Smith", title: "Product Manager", image: "https://static.vecteezy.com/system/resources/previews/029/891/975/non_2x/business-american-man-in-light-cream-ai-generative-free-photo.jpg" },
+    { name: "Alex ", title: "Head of Marketing", image: "https://images.unsplash.com/photo-1592009309602-1dde752490ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" },
+    { name: "Sarah ", title: "Customer Support Lead", image: "https://www.excelebiz.in/wp-content/uploads/2024/03/sara-tendulkar-latest-image.webp" },
+];
+
+export const MOCK_USER = {
+    email: 'ak@gmail.com',
+    name: 'Ak',
+    password: 'password123'
+};
